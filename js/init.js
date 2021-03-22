bd5ab188945568fbaae88da32b4df1d89667141b
Brandum100 <81193448+Brandum100@users.noreply.github.com>
2021-03-22 16:30:26 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+apiKey: "AIzaSyCO0w4Xiaztm7RmoLkVChSL6b3usmvMYnI",
+    authDomain: "autentifica43omb.firebaseapp.com",
+    projectId: "autentifica43omb",
+    storageBucket: "autentifica43omb.appspot.com",
+    messagingSenderId: "1046401459052",
+    appId: "1:1046401459052:web:fdbb66638dcfc650038949",
+    measurementId: "G-T1FHB16JB3"
+    });
