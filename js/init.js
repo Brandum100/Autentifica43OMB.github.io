37721d8647c2c2e633253790eaf2405019cd1c98
Brandum100 <81193448+Brandum100@users.noreply.github.com>
2021-03-22 16:33:07 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,5 +1,8 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
 firebase.initializeApp({
-apiKey: "AIzaSyCO0w4Xiaztm7RmoLkVChSL6b3usmvMYnI",
+   apiKey: "AIzaSyCO0w4Xiaztm7RmoLkVChSL6b3usmvMYnI",
     authDomain: "autentifica43omb.firebaseapp.com",
     projectId: "autentifica43omb",
     storageBucket: "autentifica43omb.appspot.com",
@@ -7,3 +10,4 @@ apiKey: "AIzaSyCO0w4Xiaztm7RmoLkVChSL6b3usmvMYnI",
     appId: "1:1046401459052:web:fdbb66638dcfc650038949",
     measurementId: "G-T1FHB16JB3"
     });
+
