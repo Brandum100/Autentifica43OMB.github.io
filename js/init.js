8e9c3b42cfc15d3528ba628d35772cd205c6e402
Brandum100 <81193448+Brandum100@users.noreply.github.com>
2021-03-25 14:00:04 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,12 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDnhDqS3SGGdWGQBt3_06bl9RrVeRU4DQE",
-    authDomain: "autentifica43omb-a3023.firebaseapp.com",
-    projectId: "autentifica43omb-a3023",
-    storageBucket: "autentifica43omb-a3023.appspot.com",
-    messagingSenderId: "949658339877",
-    appId: "1:949658339877:web:7d581d98ea2eb707994dcc",
-    measurementId: "G-HVL7KDZEKV"
+ apiKey: "AIzaSyDwtzhKXQgYSl3z6TkpPvhS8pFtme_FGpk",
+    authDomain: "chat43omb.firebaseapp.com",
+    projectId: "chat43omb",
+    storageBucket: "chat43omb.appspot.com",
+    messagingSenderId: "119852854456",
+    appId: "1:119852854456:web:33ddae140dc937cb6dd84d",
+    measurementId: "G-W0PBBKNP9P"
     });
 
